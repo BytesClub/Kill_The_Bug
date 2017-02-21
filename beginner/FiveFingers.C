53b46778f211d2e286670ffbbe91fd6d2d388ef7
Progyan Bhattacharya <bprogyan@gmail.com>
2017-02-21 16:33:46 +0530

    Update and rename prob_5.c to FiveFingers.C

---
+++ beginner/FiveFingers.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a = 5, b = 10, c = 2, d = 7;
+	printf("%d %d %d %d", a, d, c);
+	return 0;
+}
--- beginner/prob_5.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-	int a=5,b=10,c=2,d=7;
-	printf("%d %d\n",a,b,c);
-	printf("%d %d %d %d\n",a,d,c);
-	return 0;
-}
